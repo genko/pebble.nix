5b6f7407dc5adfa9a443dc5103b1ef6748e6dd31
Alexander Pärsch <alexander.paersch@gmail.com>
2025-09-08 16:22:50 +0000

    Update hash

--- derivations/pebble-toolchain-bin.nix
+++ derivations/pebble-toolchain-bin.nix
@@ -18,7 +18,7 @@ stdenv.mkDerivation (finalAttrs: {
     (rec {
       x86_64-linux = fetchzip {
         url = "https://rebble-sdk.s3-us-west-2.amazonaws.com/pebble-sdk-${finalAttrs.version}-linux64.tar.bz2";
-        hash = "14jc95zqbbl8jd8mivh44bqqvnm02lsm8jp9b3sjkrbgxlnbw0rm";
+        hash = "sha256-14jc95zqbbl8jd8mivh44bqqvnm02lsm8jp9b3sjkrbgxlnbw0rm";
       };
       x86_64-darwin = fetchzip {
         url = "https://rebble-sdk.s3-us-west-2.amazonaws.com/pebble-sdk--${finalAttrs.version}-mac.tar.bz2";
